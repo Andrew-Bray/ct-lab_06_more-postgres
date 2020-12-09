65379c941dfc10f2edbe5150d632fe419a0f18c7
Andrew-Bray <amichaelbray@gmail.com>
2020-12-08 18:44:52 -0800

    passing tests for POST and GET

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const { setupMaster } = require('cluster');
+
 const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
@@ -7,12 +7,51 @@ const pool = require('../lib/utils/pool');
 
 describe('these are my app tests', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('../lib/utils/setup.sql', 'utf-8'));
+    return pool.query(fs.readFileSync('./lib/utils/setup.sql', 'utf-8'));
   });
 
   afterAll(() => {
     return pool.end();
   });
 
-  it()
-})
+  it('should post a new chili in the database and return it', async() => {
+    const res = await request(app)
+      .post('/chilis')
+      .send({
+        brand: 'hormel',
+        description: 'not bad, add ketchup',
+        image_url: 'http://www.hormelnstuff.com'
+      });
+
+    expect(res.body).toEqual({
+      id: expect.anything(),
+      brand: 'hormel',
+      description: 'not bad, add ketchup',
+      image_url: 'http://www.hormelnstuff.com'
+    });
+  });
+
+  it('should get one chili that is in the database', async() => {
+    const res = await request(app)
+      .post('/chilis')
+      .send({
+        brand: 'stagg',
+        description: 'yummmm',
+        image_url: 'http://www.hormelnstuff.com'
+      });
+      
+    const myChiliId = res.body.id;
+
+    const newRes = await request(app)
+      .get(`/chilis/${myChiliId}`);
+    expect(newRes.body).toEqual({
+      id: expect.anything(),
+      brand: 'stagg',
+      description: 'yummmm',
+      image_url: 'http://www.hormelnstuff.com'
+    });
+  
+      
+
+  });
+});
